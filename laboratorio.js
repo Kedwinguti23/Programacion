b0e256b9edd6d99a024fb0af99ca2e4bb018e0fe
KedwinGutiérrez <kedwingutivelasquez45@gmail.com>
2024-08-25 15:09:17 -0600

    laboratorio.js

--- laboratorio.js
+++ laboratorio.js
@@ -3,13 +3,13 @@ let camisetas = 120;
 let pantalones = 90;
 let zapatos = 100;
 
-// Calcular el total de productos en el inventario
+// Calculo de el total de productos en el inventario
 let inventario = camisetas + pantalones + zapatos;
 
-// Imprimir el total de productos en el inventario
+// el total de productos en el inventario impreso en el terminal
 console.log("Total de productos en el inventario: " + inventario);
 
-// Verificar si es necesario reponer algún producto
+// Verificación para ver si es necesario reponer algún producto
 if (camisetas < 100) {
     console.log("Es necesario reponer camisetas.");
 } else if (pantalones < 100) {
